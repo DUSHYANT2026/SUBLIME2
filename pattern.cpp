4e0553c76e61001b345e2cb5df0906c58300f9b2
Dushyant2026 <dushyantpandit799@gmail.com>
2024-08-12 12:54:42 +0530

    mark all the chnages

--- pattern.cpp
+++ pattern.cpp
@@ -1,13 +1,13 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-// int n,m;
-// cin>>n>>m;
-// for(int i=0;i<=n;i++){
-// for(int j=0;j<=m;j++){
-// cout<<"*";}
-// cout<<endl;}	
-// return 0;}
+#include<iostream>
+using namespace std;
+int main(){
+int n,m;
+cin>>n>>m;
+for(int i=0;i<=n;i++){
+for(int j=0;j<=m;j++){
+cout<<"*";}
+cout<<endl;}	
+return 0;}
 
 // #include<iostream>
 // using namespace std;
@@ -217,20 +217,20 @@
 // cout<<endl;}
 // return 0;}
 
-#include<iostream>
-using namespace std;
-int main(){
-int n;
-cin>>n;
-for(int i=1;i<=n;i++){
-for(int j=1;j<=i;j++)
-cout<<j;
-for(int j=0;j<=2*n-2*i-1;j++)
-cout<<" ";
-for(int j=i;j>0;j--)
-cout<<j;    
-cout<<endl;}
-return 0;}
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cin>>n;
+// for(int i=1;i<=n;i++){
+// for(int j=1;j<=i;j++)
+// cout<<j;
+// for(int j=0;j<=2*n-2*i-1;j++)
+// cout<<" ";
+// for(int j=i;j>0;j--)
+// cout<<j;    
+// cout<<endl;}
+// return 0;}
 
 // #include<iostream>
 // using namespace std;
