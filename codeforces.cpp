2e5fce66553103860912fd37dae1978062af08ca
Dushyant <dushyantpandit799@gmail.com>
2024-08-24 00:35:31 +0530

    mark all the changes

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,17 +1,17 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n,a,b,c;
-//     cin>>n;
-//     for(int i=1;i<=n;i++){
-//     cin>>a>>b>>c;
-//     if(a+b == c) cout<<"YES\n";
-//     else if(b+c == a) cout<<"YES\n";
-//     else if(c+a == b) cout<<"YES\n";
-//     else cout<<"NO\n";
-// }
-// return 0;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int n,a,b,c;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+    cin>>a>>b>>c;
+    if(a+b == c) cout<<"YES\n";
+    else if(b+c == a) cout<<"YES\n";
+    else if(c+a == b) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+return 0;
+}
 
 
 
